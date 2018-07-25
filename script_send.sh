52202b3b3c59374997ccd78ddb70e34875384aa6
sergiiglad <sergii.gladchenko@gmail.com>
2018-07-25 15:07:13 +0300

    Update script_send.sh

--- script_send.sh
+++ script_send.sh
@@ -5,11 +5,11 @@ while true; do
 sleep $superperiod
 
 ls $superfile
-if [ "$?" -ne 0 ]; then 
-continue
-else
 
-ping -c1 -W100 $superip 
+if [ "$?" -ne 0 ]; then 
+	continue
+else
+	ping -c1 -W100 $superip 
 
 	if [ "$?" -eq 0 ]; then 
 		echo "Sending file...."
